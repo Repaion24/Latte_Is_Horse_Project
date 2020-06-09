a1578da2c02394ef87fdd3676644af0810b364d9
sdmno <65167123+sdmno@users.noreply.github.com>
2020-06-09 20:07:43 +0900

    재희

--- Opensource/tower!!.py
+++ Opensource/tower!!.py
@@ -37,8 +37,9 @@ build = 0
 build_ok = True
 game_timer = time.time()
 old_time = game_timer
-gold = 0
+gold = 1000
 round123 = 0
+font = pygame.font.Font(None, 26)
 
 
 while not is_end :
@@ -48,7 +49,7 @@ while not is_end :
         if event.type == pygame.MOUSEBUTTONDOWN :
             position = pygame.mouse.get_pos()
 
-            if build == 0 :
+            if build == 0 :                 #made by 김재희~
                 for i in range(0, len(tower1)):
                     if tower1[i].selected:
                         if position[1] >= 650 and position[1] <= 650 + tower1[i].sell.get_height():
@@ -66,7 +67,12 @@ while not is_end :
                                 index -= 1
                                 break
                             if position[0] >= 620 and position[0] <= 620 + tower1[i].upgrade.get_width():
-                                gold -= tower1[i].upgrade_tower()
+                                if tower1[i].level <= 2 :
+                                    if gold >= tower1[i].upgrade_price[tower1[i].level] :
+                                        if tower1[i].level == 2 :
+                                            tower1[i].upgrade_tower()
+                                        else :
+                                            gold -= tower1[i].upgrade_tower()
                         else:
                             tower1[i].selected = tower1[i].select_tower(position[0], position[1])
                     else:
@@ -74,29 +80,37 @@ while not is_end :
 
                 if position[0]>=1100 and position[0]<=1100+timg[0].get_width() and\
                         position[1]>=120 and position[1]<=120+timg[0].get_height() :
-                    index += 1
-                    build += 1
-                    tower1.append(tower())
-                    tower1[index].timer = time.time()
+                    if gold >= 50 :
+                        gold -= 50
+                        index += 1
+                        build += 1
+                        tower1.append(tower())
+                        tower1[index].timer = time.time()
                 if position[0]>=1180 and position[0]<=1180+timg[1].get_width() and\
                         position[1]>=120 and position[1]<=120+timg[1].get_height() :
-                    index += 1
-                    build += 1
-                    tower1.append(short_tower())
-                    tower1[index].timer = time.time()
+                    if gold >= 100 :
+                        gold -= 100
+                        index += 1
+                        build += 1
+                        tower1.append(short_tower())
+                        tower1[index].timer = time.time()
                 if position[0]>=1100 and position[0]<=1100+timg[2].get_width() and\
                         position[1]>=200 and position[1]<=200+timg[2].get_height() :
-                    index += 1
-                    build += 1
-                    tower1.append(long_tower())
-                    tower1[index].timer = time.time()
+                    if gold >= 100 :
+                        gold -= 100
+                        index += 1
+                        build += 1
+                        tower1.append(long_tower())
+                        tower1[index].timer = time.time()
                 if position[0]>=1180 and position[0]<=1180+timg[3].get_width() and\
                         position[1]>=200 and position[1]<=200+timg[3].get_height() :
-                    index += 1
-                    build += 1
-                    tower1.append(support_tower())
-                    support_index.append(index)
-                    tower1[index].timer = time.time()
+                    if gold >= 100 :
+                        gold -= 100
+                        index += 1
+                        build += 1
+                        tower1.append(support_tower())
+                        support_index.append(index)
+                        tower1[index].timer = time.time()
                 if position[0] >= 1100 and position[0] <= 1100 + eimg.get_width() and\
                         position[1] >= 400 and position[1] <= 400 + eimg.get_height():
                     eindex += 1
@@ -117,7 +131,7 @@ while not is_end :
                     position[1]>=520 and position[1]<=520 + cancel_img.get_height() :
                     index -= 1
                     tower1.pop()
-            elif build == 2 :
+            elif build == 2 :                       #~made by 김재희
                 build -= 2
                 if position[0]>=1100 and position[0]<=1100+eimg.get_width() and\
                         position[1]>=300 and position[1]<=300+eimg.get_height() :
@@ -128,15 +142,13 @@ while not is_end :
 
     screen.fill(BLACK)
 
-    if build == 1 :
+    if build == 1 :     #made by 김재희~
         position = pygame.mouse.get_pos()
         tower1[index].build_tower(position[0], position[1])
-
     elif build == 2 :
         position = pygame.mouse.get_pos()
         enemy1[eindex].x = position[0]
         enemy1[eindex].y = position[1]
-
     for i in range(0, len(tower1)) :
         attack_on = False
         if len(enemy1) > 0 :
@@ -158,7 +170,6 @@ while not is_end :
                 tower1[i].attack = 0
         if attack_on == False :
             tower1[i].attack = 0
-
     if build == 0:
         for i in support_index :
             exist = False
@@ -170,8 +181,7 @@ while not is_end :
                         exist = True
                         tower1[i].tower_attack(game_timer)
             if exist == False :
-                tower1[i].attack = 0
-
+                tower1[i].attack = 0            #~made by 김재희
 
     for i in range(0, len(tower1)) :
         tower1[i].blit_tower(screen)
@@ -179,6 +189,8 @@ while not is_end :
     for i in range(0, len(enemy1)) :
         screen.blit(enemy1[i].enemy_img, (enemy1[i].x - enemy1[i].enemy_img.get_width()/2, enemy1[i].y - enemy1[i].enemy_img.get_height()/2))
 
+    gold_font = font.render(str(gold), True, (255, 255, 255))
+    screen.blit(gold_font, (1180, 40))
     screen.blit(timg[0], (1100, 120))
     screen.blit(timg[1], (1180, 120))
     screen.blit(timg[2], (1100, 200))
